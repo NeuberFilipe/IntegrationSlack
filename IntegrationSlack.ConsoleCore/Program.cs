3c8f2d71442dad3f2cc2b3577702730911a4272e
BNK SYSTEM <bnksystem@outlook.com>
2018-08-30 11:47:11 -0300

    Integration Console.Core on Slack.

---
+++ IntegrationSlack.ConsoleCore/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using SlackBotMessages;
+
+namespace IntegrationSlack.ConsoleCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SBMClient client = new SBMClient("https://hooks.slack.com/services/Your/WebHook/Url");
+            Message msg = new Message(text:"Seja bem vindo! Jamelão "+ DateTime.Now.ToShortDateString() ,channel: "general", username:"GTS", icon_emoji: ":sunglasses:");
+            client.Send(msg);
+        }
+    }
+}
